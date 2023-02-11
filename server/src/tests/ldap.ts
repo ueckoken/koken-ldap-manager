69f04eb420e2a9ebcd55ca064b2982836941b381
VOID <kouichi.hirachi@gmail.com>
2023-02-11 14:54:39 +0900

    add build test

--- server/src/tests/ldap.ts
+++
@@ -1,7 +0,0 @@
-import { addUserToGroup, getAllgroups, getUserGroup, removeUserFromGroup } from "../models/GroupModel";
-import { getAllusers, getUser } from "../models/UserModel";
-
-async function main() {
-  // Test
-}
-main();
